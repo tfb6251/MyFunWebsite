77c5cb60a136d68c5afeaf92234d4a7e7de21af1
hl8678 <159577405+hl8678@users.noreply.github.com>
2024-09-30 18:05:55 -0400

    ok

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/ComputerController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/ComputerController.java
@@ -18,8 +18,26 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 //import com.heroes.api.heroesapi.persistence.HeroDAO;
+
 //import com.heroes.api.heroesapi.model.Hero;
 
 public class ComputerController {
-    
+    /**
+     * Creates a {@linkplain Hero hero} with the provided hero object
+     * 
+     * @param hero - The {@link Hero hero} to create
+     * 
+     * @return ResponseEntity with created {@link Hero hero} object and HTTP status of CREATED<br>
+     * ResponseEntity with HTTP status of CONFLICT if {@link Hero hero} object already exists<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    /* 
+    @PostMapping("")
+    public ResponseEntity<Needs> createNeeds(@RequestBody Needs hero) {
+        LOG.info("POST /heroes " + hero);
+
+        // Replace below with your implementation
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }*/
 }
+
