68f565300d3aef5538ce5fc2a84d5e3b55931bb6
tfb6251 <124169655+tfb6251@users.noreply.github.com>
2024-10-21 20:52:22 -0400

    Update CupBoardFileDAOTest.java
    Implemented Cupboard tests

--- ufund-api/src/test/java/com/ufund/api/ufundapi/persistence/CupBoardFileDAOTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/persistence/CupBoardFileDAOTest.java
@@ -22,5 +22,156 @@ import org.junit.jupiter.api.Test;
 
 
 public class CupBoardFileDAOTest {
+    CupBoardFileDAO cupBoardFileDAO;
+    Needs[] testNeeds;
+    ObjectMapper mockObjectMapper;
+
+
+    @BeforeEach
+    public void setupHeroFileDAO() throws IOException {
+        mockObjectMapper = mock(ObjectMapper.class);
+        testNeeds = new Needs[3];
+        testNeeds[0] = new Needs("Obiwan", 1000000, 1000, "Toshiba" );
+        testNeeds[1] = new Needs("QuiGon", 100000, 100, "Toyota" );
+        testNeeds[2] = new Needs("Yoda", 10000, 10, "Honda" );
+
+
+        when(mockObjectMapper
+            .readValue(new File("doesnt_matter.txt"),Needs[].class))
+                .thenReturn(testNeeds);
+        cupBoardFileDAO = new CupBoardFileDAO("doesnt_matter.txt",mockObjectMapper);
+    }
+
+    @Test //1
+    public void testGetHeroes() {
+
+        Needs[] heroes = cupBoardFileDAO.getNeedsArray();
+
+
+        assertEquals(heroes.length,testNeeds.length);
+        for (int i = 0; i < testNeeds.length;++i)
+            assertEquals(heroes[i],testNeeds[i]);
+    }
+
+    @Test //2
+    public void testFindHeroes() {
+
+        Needs[] heroes = cupBoardFileDAO.findNeeds("n");
+
+
+        assertEquals(heroes.length,2);
+        assertEquals(heroes[0],testNeeds[1]);
+        assertEquals(heroes[1],testNeeds[2]);
+    }
+
+    @Test //3
+    public void testGetHero() {
     
+        Needs hero = cupBoardFileDAO.getNeed("Obiwan");
+
+
+        assertEquals(hero,testNeeds[0]);
+    }
+
+    @Test //4
+    public void testDeleteHero() {
+
+        boolean result = assertDoesNotThrow(() -> cupBoardFileDAO.deleteNeeds("Obiwan"),
+                            "Unexpected exception thrown");
+
+
+        assertEquals(result,true);
+
+        assertEquals(cupBoardFileDAO.computer.size(),testNeeds.length-1);
+    }
+
+    @Test //5
+    public void testCreateHero() {
+        
+        Needs need = new Needs("MaceWindu", 1000, 1, "Bugatti" );
+
+        
+        Needs result = assertDoesNotThrow(() -> cupBoardFileDAO.createNeeds(need),
+                                "Unexpected exception thrown");
+
+        
+        assertNotNull(result);
+        Needs actual = cupBoardFileDAO.getNeed(need.getName());
+        assertEquals(actual.getName(),need.getName());
+    }
+
+    @Test //6
+    public void testUpdateHero() {
+
+        Needs need = new Needs("Obiwan", 10000000, 10000, "HelloThere" );;
+
+
+        Needs result = assertDoesNotThrow(() -> cupBoardFileDAO.updateNeeds(need),
+                                "Unexpected exception thrown");
+
+
+        assertNotNull(result);
+        Needs actual = cupBoardFileDAO.getNeed(need.getName());
+        assertEquals(actual,need);
+    }
+
+    @Test //7
+    public void testSaveException() throws IOException{
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .writeValue(any(File.class),any(Needs[].class));
+
+        Needs need = new Needs("MaceWindu", 1000, 1, "Bugatti" );
+
+        assertThrows(IOException.class,
+                        () -> cupBoardFileDAO.createNeeds(need),
+                        "IOException not thrown");
+    }
+
+    @Test //8
+    public void testGetHeroNotFound() {
+
+        Needs hero = cupBoardFileDAO.getNeed("any");
+
+
+        assertEquals(hero,null);
+    }
+
+    @Test //9
+    public void testDeleteHeroNotFound() {
+
+        boolean result = assertDoesNotThrow(() -> cupBoardFileDAO.deleteNeeds("any"),
+                                                "Unexpected exception thrown");
+
+
+        assertEquals(result,false);
+        assertEquals(cupBoardFileDAO.computer.size(),testNeeds.length);
+    }
+
+    @Test //10
+    public void testUpdateHeroNotFound() {
+
+        Needs need = new Needs("MaceWindu", 1000, 1, "Bugatti" );
+
+
+        Needs result = assertDoesNotThrow(() -> cupBoardFileDAO.updateNeeds(need),
+                                                "Unexpected exception thrown");
+
+
+        assertNull(result);
+    }
+
+    @Test //11
+    public void testConstructorException() throws IOException {
+
+        ObjectMapper mockObjectMapper = mock(ObjectMapper.class);
+
+        doThrow(new IOException())
+            .when(mockObjectMapper)
+                .readValue(new File("doesnt_matter.txt"),Needs[].class);
+
+        assertThrows(IOException.class,
+                        () -> new CupBoardFileDAO("doesnt_matter.txt",mockObjectMapper),
+                        "IOException not thrown");
+    }
 }
